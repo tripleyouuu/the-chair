316a0ad890504cbdd7685ca2aae8015b69430217
Aurora <apurnawan75@gmail.com>
2026-08-12 00:54:30 +0800

    Split model into one type per file for readability

--- the chair/the chair/Model/ClothingItem.swift
+++ the chair/the chair/Model/ClothingItem.swift
@@ -2,68 +2,10 @@
 //  ClothingItem.swift
 //  
 //
-//  Created by Aurora Purnawan on 11/08/26.
+//  Created by Aurora Purnawan on 12/08/26.
 //
 
 import Foundation
-
-enum ClothingMaterial: String, CaseIterable, Codable, Identifiable {
-    case dryFit = "Dry-Fit"
-    case silk = "Silk"
-    case cottonLinen = "Cotton/Linen"
-    case nylonPolyester = "Nylon/Polyester"
-    case denim = "Denim"
-    case wool = "Wool"
-
-    var id: String { rawValue }
-
-    var baseHours: Double {
-        switch self {
-        case .dryFit, .silk: return 0
-        case .cottonLinen: return 16
-        case .nylonPolyester: return 12
-        case .denim: return 48
-        case .wool: return 56
-        }
-    }
-}
-
-enum ClothingCategory: String, CaseIterable, Codable, Identifiable {
-    case top = "Top"
-    case bottom = "Bottom"
-    case singlePiece = "Single-Piece"
-    case outerWear = "Outerwear"
-
-    var id: String { rawValue }
-
-    var wearabilityMultiplier: Double {
-        switch self {
-        case .top, .singlePiece: return 1.0
-        case .bottom: return 1.5
-        case .outerWear: return 5.0
-        }
-    }
-}
-
-enum ClothingColor: String, CaseIterable, Codable, Identifiable {
-    case white = "White"
-    case black = "Black"
-    case gray = "Gray"
-    case red = "Red"
-    case yellow = "Yellow"
-    case orange = "Orange"
-    case green = "Green"
-    case blue = "Blue"
-    case purple = "Purple"
-    case pink = "Pink"
-
-    var id: String { rawValue }
-
-    func multiplier(with clothingMaterial: ClothingMaterial) -> Double {
-        guard self == .white, clothingMaterial != .denim, clothingMaterial != .wool else { return 1.0 }
-        return 0.0
-    }
-}
 
 struct ClothingItem: Identifiable, Codable {
     let id: UUID
@@ -89,7 +31,7 @@ struct ClothingItem: Identifiable, Codable {
         self.wearSessions = wearSessions
     }
 
-    // Lookup key for design's silhouette set assets, this is just the name they'll match against.
+    // Lookup key for design's silhouette set assets, this is just the name they'll match.
     var silhouetteAssetName: String {
         let name = String(describing: clothingCategory)
         return "silhouette" + name.prefix(1).uppercased() + name.dropFirst()
@@ -112,4 +54,3 @@ struct ClothingItem: Identifiable, Codable {
         return wearabilityRemaining <= 0
     }
 }
-
--- the chair/the chair/Model/WearSession.swift
+++ the chair/the chair/Model/WearSession.swift
@@ -7,46 +7,6 @@
 
 import Foundation
 
-enum ActivityLevel: String, CaseIterable, Codable, Identifiable {
-    case resting = "Resting"
-    case light = "Light"
-    case moderate = "Moderate"
-    case active = "Active"
-    case intense = "Intense"
-
-    var id: String { rawValue }
-
-    var multiplier: Double {
-        switch self {
-        case .resting: return 1.0
-        case .light: return 1.37
-        case .moderate: return 1.87
-        case .active: return 2.5
-        case .intense: return 3.5
-        }
-    }
-}
-
-enum EnvironmentLevel: String, CaseIterable, Codable, Identifiable {
-    case cold = "Cold"
-    case cool = "Cool"
-    case mild = "Mild"
-    case warm = "Warm"
-    case hot = "Hot"
-
-    var id: String { rawValue }
-
-    var multiplier: Double {
-        switch self {
-        case .cold: return 1.0
-        case .cool: return 1.25
-        case .mild: return 1.5
-        case .warm: return 1.75
-        case .hot: return 2.0
-        }
-    }
-}
-
 struct WearSession: Identifiable, Codable {
     let id: UUID
     var date: Date
