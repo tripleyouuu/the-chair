9a2b90155459c8f5b3b1d7b0fd39ff6c18660dcb
Aulia Nadhirah Yasmin Badrulkamal <aulianadhirahyb@gmail.com>
2026-08-19 20:31:52 +0800

    fix : Color picker UI and add clothes overhaul

--- the chair/the chair/Screens/AddClothesView.swift
+++ the chair/the chair/Screens/AddClothesView.swift
@@ -30,16 +30,10 @@ struct AddClothesView: View {
     var body: some View {
             ScrollView {
                 VStack(spacing:20){
-                    clothesPreview
-                    TextField("Nickname", text: $clothesNickname)
-                        .textFieldStyle(.plain)
-                        .padding()
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(.infinity)
+                    clothesPreviewNickname
                     colorSection
-                    materialSection
                     categorySection
-                    silhouetteSection
+                    materialSection
                     
                 }
                 .padding(.top, 100)
@@ -47,15 +41,9 @@ struct AddClothesView: View {
             }
             // TO USE TOAST, ADD THIS
             .toast($toast)
+            .background(Color(.systemGray6))
             .ignoresSafeArea(edges: .all)
             .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                    } label: {
-                        Image(systemName: "chevron.left")
-                    }
-                }
-
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
                         withAnimation(.spring(response: 0.4, dampingFraction: 0.75)){
@@ -69,22 +57,32 @@ struct AddClothesView: View {
             }
         }
     
-    private var clothesPreview : some View {
-        ZStack{
-            // TODO: add button to set colors
-            RoundedRectangle(cornerRadius: 16)
-                .fill(Color.gray.opacity(0.1))
-                .frame(width: 230, height: 230)
-                .overlay {
-                    Image(systemName: "tshirt")
-                        .font(.system(size: 70))
-                        .foregroundStyle(.secondary)
-                }
+    private var clothesPreviewNickname : some View {
+        VStack(spacing:16){
+            ZStack{
+                // TODO: add button to set colors
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color(.systemGray6))
+                    .frame(width: 200, height: 200)
+                    .overlay {
+                        Image(systemName: "tshirt")
+                            .font(.system(size: 70))
+                            .foregroundStyle(.secondary)
+                    }
+            }
+            TextField("Nickname", text: $clothesNickname)
+                .textFieldStyle(.plain)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(.infinity)
         }
+        .padding(16)
+        .background(.white)
+        .cornerRadius(16)
     }
     
     struct ColorPicker: View {
-        var color : ClothingColor = .red
+        var color : ClothingColor
         @Binding var selectedColor: ClothingColor
         var body : some View {
             Button {
@@ -92,10 +90,13 @@ struct AddClothesView: View {
             } label: {
                 ZStack{
                     Circle()
-                        .fill(Color.blue)
+                        .fill(Color(color.swiftUIColor))
                         .frame(width: 32)
-                    Circle().stroke(.white, lineWidth: color == .white ? 2 : 0)
-                    Circle().stroke(Color(.systemGray2), lineWidth: color == selectedColor ? 4 : 0)
+                    Circle()
+                        .stroke(Color(.systemGray4), lineWidth: color == .white ? 2 : 0)
+                        .frame(width: 32)
+                    Circle()
+                        .stroke(Color(.systemGray2), lineWidth: color == selectedColor ? 4 : 0)
                         .frame(width: 40)
                 }
             }
@@ -104,21 +105,29 @@ struct AddClothesView: View {
     
     private var colorSection : some View {
         VStack(alignment: .leading){
-            Text("COLOR")
-                .font(.headline)
-            LazyVGrid(columns:[GridItem(.adaptive(minimum: 38))], alignment: .center){
+            VStack(alignment: .leading){
+                Text("Color")
+                    .font(.headline)
+                Text(clothesColor.rawValue)
+            }
+            LazyVGrid(columns:[GridItem(.adaptive(minimum: 34))], alignment: .center){
                 ForEach(ClothingColor.allCases) {
                     color in
                     ColorPicker(color: color, selectedColor: $clothesColor)
                 }
             }
+            .padding()
+            .background()
+            .cornerRadius(16)
         }
     }
     
     private var materialSection: some View {
         VStack(alignment: .leading, spacing: 8){
-            Text("MATERIAL")
-                .font(.headline)
+            VStack{
+                Text("Material")
+                    .font(.headline)
+            }
             LazyVGrid(columns:columns, alignment: .leading){
                 ForEach(ClothingMaterial.allCases) {
                     material in
@@ -136,49 +145,56 @@ struct AddClothesView: View {
                             .foregroundStyle(
                                 clothesMaterial == material
                                 ? .white
-                                : Color.accentColor
+                                : Color(.systemGray)
                             )
                             .clipShape(RoundedRectangle(cornerRadius: .infinity))
                     }
                 }
             }
+            .padding()
+            .background()
+            .cornerRadius(16)
         }
     }
     
     private var categorySection: some View {
         VStack(alignment: .leading, spacing: 8){
-            Text("CATEGORY")
+            Text("Silhouette")
                 .font(.headline)
-            LazyVGrid(columns:[GridItem(.adaptive(minimum: 150))], alignment: .leading){
-                ForEach(ClothingCategory.allCases) {
-                    category in
-                    Button(action: {
-                        self.clothesCategory = category
-                    }){
-                        Text(category.rawValue)
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 8)
-                            .background(
-                                clothesCategory == category
+            VStack(spacing:20){
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 150))], alignment: .leading){
+                    ForEach(ClothingCategory.allCases) {
+                        category in
+                        Button(action: {
+                            self.clothesCategory = category
+                        }){
+                            Text(category.rawValue)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 8)
+                                .background(
+                                    clothesCategory == category
                                     ? Color.accentColor
-                                : Color(.systemGray5)
-                            )
-                            .foregroundStyle(
-                                clothesCategory == category
-                                ? .white
-                                : Color.accentColor
-                            )
-                            .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                                    : Color(.systemGray5)
+                                )
+                                .foregroundStyle(
+                                    clothesCategory == category
+                                    ? .white
+                                    : Color(.systemGray)
+                                )
+                                .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                        }
                     }
                 }
+                silhouetteSection
             }
+            .padding()
+            .background()
+            .cornerRadius(16)
         }
     }
     
     private var silhouetteSection: some View {
         VStack(alignment: .leading, spacing: 8){
-            Text("SILHOUETTE")
-                .font(.headline)
             LazyVGrid(columns:[GridItem(.adaptive(minimum: 100))], alignment: .center){
                 ForEach(silhouettesOptions){
                     silhouettes in
@@ -191,15 +207,11 @@ struct AddClothesView: View {
                                 .frame(width: .infinity, height:120)
                                 .foregroundStyle(clothesSilhouettes == silhouettes ? Color.accentColor : Color(.systemGray5))
                                 .cornerRadius(16)
-//                                    Image(systemName: "tshirt.fill")
-//                                        .resizable()
-//                                        .scaledToFit()
-//                                        .frame(width: 80)
                             Text(silhouettes.rawValue)
                             .foregroundStyle(
                                 clothesSilhouettes == silhouettes
                                 ? .white
-                                : Color.accentColor
+                                : Color(.systemGray)
                             )
                         }
 
