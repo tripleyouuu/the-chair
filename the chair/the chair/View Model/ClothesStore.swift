04f17e3b26d2461c378871ee3e78f912d0f8a7bc
Vitha <emailvitha@gmail.com>
2026-08-19 11:50:27 +0800

    add: reference image support + refactored mock data

--- the chair/the chair/View Model/ClothesStore.swift
+++ the chair/the chair/View Model/ClothesStore.swift
@@ -1,6 +1,6 @@
 //
 //  ClothesStore.swift
-//  
+//
 //
 //  Created by Aurora Purnawan on 12/08/26.
 //
@@ -23,31 +23,128 @@ final class ClothesStore: ObservableObject {
     }
 
     // TODO: make it so that data for different places are placed in separate arrays so that the pile, closet and wash clothes have different arrays
+
     func seedMockData() {
         clothes = [
-            ClothingItem(nickname: "Everyday Tee", clothingMaterial: .natural, clothingColor: .white, silhouette: .tShirt, location: .pile),
-            ClothingItem(nickname: "Work Jeans", clothingMaterial: .denim, clothingColor: .darkBlue, silhouette: .pants, location: .pile),
-            ClothingItem(nickname: "Rain Jacket", clothingMaterial: .synthetic, clothingColor: .black, silhouette: .jacket, location: .pile),
-            ClothingItem(nickname: "Sunday Sweater", clothingMaterial: .wool, clothingColor: .gray, silhouette: .hoodie, location: .pile),
-            ClothingItem(nickname: "Slip Dress", clothingMaterial: .silk, clothingColor: .red, silhouette: .dress, location: .pile),
+            ClothingItem(
+                nickname: "Everyday Tee",
+                clothingMaterial: .natural,
+                clothingColor: .white,
+                silhouette: .tShirt,
+                location: .pile,
+                referenceImageName: "testCircle"
+            ),
+            ClothingItem(
+                nickname: "Work Jeans",
+                clothingMaterial: .denim,
+                clothingColor: .darkBlue,
+                silhouette: .pants,
+                location: .pile
+            ),
+            ClothingItem(
+                nickname: "Rain Jacket",
+                clothingMaterial: .synthetic,
+                clothingColor: .black,
+                silhouette: .jacket,
+                location: .pile,
+                referenceImageName: "testCircle"
+            ),
+            ClothingItem(
+                nickname: "Sunday Sweater",
+                clothingMaterial: .wool,
+                clothingColor: .gray,
+                silhouette: .hoodie,
+                location: .pile
+            ),
+            ClothingItem(
+                nickname: "Slip Dress",
+                clothingMaterial: .silk,
+                clothingColor: .red,
+                silhouette: .dress,
+                location: .pile
+            )
         ]
+
         persistence.save(clothes)
     }
-    
+
     // TODO: delete this function later
     func seedMockCloset() {
         closetClothes = [
-            ClothingItem(nickname: "Everyday Tee", clothingMaterial: .natural, clothingColor: .white, silhouette: .tShirt, location: .wardrobe),
-            ClothingItem(nickname: "Work Jeans", clothingMaterial: .denim, clothingColor: .darkBlue, silhouette: .pants, location: .wardrobe),
-            ClothingItem(nickname: "Rain Jacket", clothingMaterial: .synthetic, clothingColor: .black, silhouette: .jacket, location: .wardrobe),
-            ClothingItem(nickname: "Sunday Sweater", clothingMaterial: .wool, clothingColor: .gray, silhouette: .hoodie, location: .wardrobe),
-            ClothingItem(nickname: "Slip Dress", clothingMaterial: .silk, clothingColor: .red, silhouette: .dress, location: .wardrobe),
-            ClothingItem(nickname: "Everyday Tee", clothingMaterial: .natural, clothingColor: .white, silhouette: .tShirt, location: .wardrobe),
-            ClothingItem(nickname: "Work Jeans", clothingMaterial: .denim, clothingColor: .darkBlue, silhouette: .pants, location: .wardrobe),
-            ClothingItem(nickname: "Rain Jacket", clothingMaterial: .synthetic, clothingColor: .black, silhouette: .jacket, location: .wardrobe),
-            ClothingItem(nickname: "Sunday Sweater", clothingMaterial: .wool, clothingColor: .gray, silhouette: .hoodie, location: .wardrobe),
-            ClothingItem(nickname: "Slip Dress", clothingMaterial: .silk, clothingColor: .red, silhouette: .dress, location: .wardrobe)
+            ClothingItem(
+                nickname: "Everyday Tee",
+                clothingMaterial: .natural,
+                clothingColor: .white,
+                silhouette: .tShirt,
+                location: .wardrobe,
+                referenceImageName: "testCircle"
+            ),
+            ClothingItem(
+                nickname: "Work Jeans",
+                clothingMaterial: .denim,
+                clothingColor: .darkBlue,
+                silhouette: .pants,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Rain Jacket",
+                clothingMaterial: .synthetic,
+                clothingColor: .black,
+                silhouette: .jacket,
+                location: .wardrobe,
+                referenceImageName: "testCircle"
+            ),
+            ClothingItem(
+                nickname: "Sunday Sweater",
+                clothingMaterial: .wool,
+                clothingColor: .gray,
+                silhouette: .hoodie,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Slip Dress",
+                clothingMaterial: .silk,
+                clothingColor: .red,
+                silhouette: .dress,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Everyday Tee",
+                clothingMaterial: .natural,
+                clothingColor: .white,
+                silhouette: .tShirt,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Work Jeans",
+                clothingMaterial: .denim,
+                clothingColor: .darkBlue,
+                silhouette: .pants,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Rain Jacket",
+                clothingMaterial: .synthetic,
+                clothingColor: .black,
+                silhouette: .jacket,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Sunday Sweater",
+                clothingMaterial: .wool,
+                clothingColor: .gray,
+                silhouette: .hoodie,
+                location: .wardrobe
+            ),
+            ClothingItem(
+                nickname: "Slip Dress",
+                clothingMaterial: .silk,
+                clothingColor: .red,
+                silhouette: .dress,
+                location: .wardrobe
+            )
         ]
+
         persistence.save(closetClothes)
     }
 }
